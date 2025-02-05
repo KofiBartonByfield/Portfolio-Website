16e8f44c81948ba4cb630e95afbfddba6551636b
Kofi Barton-Byfield <kbartonbyfield@gmail.com>
2025-02-05 20:18:36 +0000

    na

--- Java_Scripts/header.js
+++ Java_Scripts/header.js
@@ -1,23 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("header-placeholder").innerHTML = `
         <header class="header-container">
+
             <a href="home.html" class="no-underline">
                 <h1 class="title-text">
-                <span aria-label="Search">&#9816;</span>
-                Portfolio</h1>
+                    <span aria-label="Search">
+                        &#9816;
+                    </span>
+                    
+                    Portfolio
+                </h1>
             </a>
 
            
-            
-
+    
             <div class="LogoContainer">
 
+                <a href="tools.html">
+                    <div class="tools_title">
+                        <i class="fa fa-wrench" aria-hidden="true"></i>
+                    </div> 
+                </a>
 
-            <a href="tools.html">
-                <div class="tools_title">
-                    <i class="fa fa-wrench" aria-hidden="true"></i>
-                </div> 
-            </a>
                 <a href="https://www.linkedin.com/in/kofi-barton-byfield-b7b83a293/" target="_blank">
                     <div class="tools_title">
                         <i class="fa fa-linkedin" aria-hidden="true"></i>
@@ -29,13 +33,7 @@ document.addEventListener("DOMContentLoaded", function() {
                         <i class="fa fa-github" aria-hidden="true"></i>
                     </div> 
                 </a>
-                <!--
-                <a href="">
-                    <div id="emailImage" class="tools_title">
-                        <i class="fa fa-envelope-o" aria-hidden="true"></i>
-                    </div> 
-                </a>
-                -->
+               
             </div>
         </header>
     `;
