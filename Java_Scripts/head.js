6def3a789c26f0c192e0541e334ee0b1880e08e6
Kofi Barton-Byfield <kbartonbyfield@gmail.com>
2025-02-05 19:08:30 +0000

    header.js

--- Java_Scripts/head.js
+++ Java_Scripts/head.js
@@ -7,6 +7,8 @@ document.write(`
     <script src="../Java_Scripts/home_page.js" defer></script>
     <script src="../Java_Scripts/projects.js" defer></script>
     <script src="../Java_Scripts/search-container.js" defer></script>
+    <script src="../Java_Scripts/header.js" defer></script>
+
     <meta name="image" property="og:image" content="preview-image.png">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel="icon" type="image/x-icon" href="../Images/Logo/apple-touch-icon.png">
